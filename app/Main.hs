b3f2153481a7034b65d26b8a2cea769905c86330
Phil de Joux <philderbeast@gmail.com>
2024-09-24 07:15:20 -0400

    Add aeson-shim

--- app/Main.hs
+++ app/Main.hs
@@ -5,6 +5,7 @@
 
 module Main (main) where
 
+import Data.Aeson.Shim (bwd)
 import Data.Aeson (FromJSON (..), ToJSON (..), Value (Number, Object), decode, encode, object, pairs, (.:), (.=))
 import qualified Data.HashMap.Strict as HM
 import Data.Text (Text)
@@ -46,42 +47,46 @@ echo c@(toJSON -> Object s) = "As object: " ++ show s ++ "\nAs coord: " ++ show 
 echo c = "As coord: " ++ show c
 
 -- | Using @-XViewPatterns@ at the top level. Imagine if the implementation is
--- more way more complex, you're making an upgrade aeson-2 and this is a
--- breaking change. Rather than use CPP and conditional compilation, with a shim
--- and top-level view patterns the implementation can stay the same except for
--- conversion of an input argument.
+-- more way more complex (instead of just calling `keyValue`, `wrongKey`, and
+-- `echo`), you're making an upgrade to aeson-2 and this is a breaking change.
+-- Rather than use CPP and conditional compilation, with a shim and top-level
+-- view patterns the implementation can stay the same except for conversion of
+-- an input argument.
 echoTopViewPatterns :: Text -> Coord -> String
-echoTopViewPatterns k (toJSON -> Object (HM.lookup k -> Just (Number v))) = keyValue k v
-echoTopViewPatterns k (toJSON -> Object x) = wrongKey k x
+echoTopViewPatterns k (toJSON -> Object (HM.lookup k . bwd -> Just (Number v))) = keyValue k v
+echoTopViewPatterns k (toJSON -> Object (bwd -> x)) = wrongKey k x
 echoTopViewPatterns _ x = echo x
 
 -- | Using @-XViewPatterns@ in a case expression. Like with
--- `echoTopViewPatterns`, if the implementation effected by a breaking change,
--- the change is isolated to the case expression then a shim can be added at
--- that point, in a pattern of the case expression rather than the top level.
+-- `echoTopViewPatterns`, if the implementation doesn't compile with the
+-- breaking aeson-2 change, this change can be isolated to the case expression
+-- pattern match by adding a shim there.
 echoCaseViewPatterns :: Text -> Coord -> String
 echoCaseViewPatterns k c =
     case toJSON c of
-        Object (HM.lookup k -> Just (Number v)) -> keyValue k v
-        Object x -> wrongKey k x
+        Object (HM.lookup k . bwd -> Just (Number v)) -> keyValue k v
+        Object (bwd -> x) -> wrongKey k x
         _ -> echo c
 
--- | Not using @-XViewPatterns@ but using @-XPatternGuards@ instead.
+-- | Not using @-XViewPatterns@ but using @-XPatternGuards@ instead, the
+-- shimming is done in the implementation. With a more complex implementation,
+-- this might have to be done in multiple places, increasing the risk of errors.
 echoPatternGuards :: Text -> Coord -> String
 echoPatternGuards k c
     | Object x <- toJSON c
-    , Just (Number v) <- HM.lookup k x =
+    , Just (Number v) <- HM.lookup k (bwd x) =
         keyValue k v
-    | Object x <- toJSON c = wrongKey k x
+    | Object x <- toJSON c = wrongKey k (bwd x)
     | otherwise = echo c
 
--- | Not using @-XViewPatterns@ or @-XPatternGuards@, only case expressions.
+-- | Not using @-XViewPatterns@ or @-XPatternGuards@, only case expressions and
+-- again, the shimming is done in the implementation.
 echoCase :: Text -> Coord -> String
 echoCase k c =
     case toJSON c of
-        Object x -> case HM.lookup k x of
+        Object x -> case HM.lookup k (bwd x) of
             Just (Number v) -> keyValue k v
-            _ -> wrongKey k x
+            _ -> wrongKey k (bwd x)
         _ -> echo c
 
 main :: IO ()
